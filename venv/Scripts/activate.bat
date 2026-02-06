fddd186d51ff64f7dbf3e51c63a1036a73166867
Sebas99-Luis <luisalvasebastian@gmail.com>
2026-02-06 16:03:30 +0100

    entregables

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\info\Documents\NORRDATA\Proyectos\dogdaycare\venv
+set VIRTUAL_ENV=C:\Users\luisa\Documents\NORRDATA\proyectos\dogdaycare\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
